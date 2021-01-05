bb52aa91c6eb8e09922c4515212fa8a382d2d5e2
Marco Iorio <marco.iorio@polito.it>
2021-01-05 15:02:27 +0100

    Add support to both plain and base64-encoded kubeconfigs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@
 set -e
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
+# If base64 decoding fails, assume the KUBECONFIG was plain text
 mkdir -p ~/.kube
-echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
+echo "${INPUT_KUBECONFIG}" | base64 -d > ~/.kube/config 2>/dev/null || \
+    echo "${INPUT_KUBECONFIG}" > ~/.kube/config
 
 # Execute kubectl command
 sh -c "kubectl $*"
